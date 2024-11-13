658815c0d249fda04b399b0e5eb96bf60418f975
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2024-11-13 14:09:31 +0100

    first commit

---
+++ register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php if(!isset($_POST['submit'])): ?>
+    <!-- pokaż tą część jeśli nie wysłaliśmy formularza -->
+    <h1>Zarejestruj nowe konto</h1>
+    <form action="register.php" method="post">
+        <!-- pole na email oraz labelka do niego -->
+        <label for="emailInput">Login:</label>
+        <input type="email" name="emailInput" id="emailInput"><br>
+        <!-- pole na hasło oraz labelka do niego -->
+        <label for="passwordInput">Hasło:</label>
+        <input type="password" name="passwordInput" id="passwordInput"><br>
+        <!-- guzik wyślij -->
+        <input type="submit" name="submit" value="Zarejestruj">
+    </form>
+    <?php else: ?>
+    <!-- pokaż tą część jeśli wysłaliśmy formularz -->
+    <?php    
+        $db = new mysqli('localhost', 'root', '', 'phploginform');
+        $email = $_POST['emailInput'];
+        $password = $_POST['passwordInput'];
+        $sql = "INSERT INTO user (email, password) VALUES ('$email','$password')";
+        $result = $db->query($sql);
+    ?>
+    <h1>Rejestracja zakończona pomyślnie</h1>
+    <a href="index.php">Powrót do głównej strony</a>
+    <?php endif; ?>
+</body>
+</html>
