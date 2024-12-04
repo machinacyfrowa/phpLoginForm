da90fa6d8eb0b1e088c4dd6d7dd66608b78237e1
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2024-12-04 15:01:41 +0100

    Add routing and session management with new .htaccess and templates for login/logout

--- index.php
+++ index.php
@@ -1,40 +1,21 @@
-<?php 
-require_once('class/User.php');
-session_start(); 
+<?php
+
+require_once('class/Route.php');
+
+use Steampixel\Route;
+
+Route::add('/', function() {
+    echo "Hello world!";
+});
+Route::add('/login', function() {
+    echo "strona logowania";
+});
+Route::add('/register', function() {
+    echo "strona rejestracji";
+});
+Route::add('/logout', function() {
+    echo "wylogowano";
+});
+
+Route::run('/phploginform');
 ?>
-<!DOCTYPE html>
-<html lang="pl">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<body>
-    <!-- kontener bootstrapa na siatkę kolumn -->
-    <div class="container">
-        <div class="col-8 col-lg-4 offset-2 offset-lg-4 text-center pt-3">
-            <h1 class="mb-5">Menu główne</h1>
-            <a href="register.php">
-                <button type="button" class="btn btn-primary btn-lg w-100 mb-5">Zarejestruj</button>
-            </a>
-            <br>
-            <a href="login.php">
-                <button type="button" class="btn btn-primary btn-lg w-100 mb-5">Zaloguj</button>
-            </a>
-            <?php if(isset($_SESSION['user'])): ?>
-                <p>Jesteś zalogowany jako: <?php echo $_SESSION['user']->getEmail(); ?></p>
-                <a href="logout.php">
-                    <button type="button" class="btn btn-primary btn-lg w-100 mb-5">Wyloguj</button>
-                </a>
-            <?php endif; ?>
-        </div>
-    </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-
-</html>
