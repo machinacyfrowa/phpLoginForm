e65604c01aa10804a6bb61d360a8bf8de01d132d
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2025-02-05 14:43:29 +0100

    Enhance image upload functionality by generating a unique filename based on the original name and current time

--- class/Image.php
+++ class/Image.php
@@ -2,10 +2,16 @@
 class Image {
 
     public static function Upload(array $files) {
+        //pobierz oryginalną nazwe pliku
         $name = $files['name'];
-        $tempName = $files['tmp_name'];
+        //wyciągnij z oryginalnej nazwy pliku rozszerzenie
+        $extension = pathinfo($name, PATHINFO_EXTENSION);
+        //wygeneruj unikatowy string na podstawie nazwy pliku i aktualnego czasu
+        $name = sha1($name.microtime().".".$extension);
+        //pobierz ścieżkę pliku tymczasowego
+        $tempPath = $files['tmp_name'];
         $path = "uploads/".$name; 
-        move_uploaded_file($tempName, $path);
+        move_uploaded_file($tempPath, $path);
     }
 }
 
