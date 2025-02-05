bf9ef0920a22510314a5f3f5345c7ab2efcc5db2
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2025-02-05 14:46:59 +0100

    Refactor image upload process to generate unique file paths and add VSCode launch configuration

--- class/Image.php
+++ class/Image.php
@@ -7,10 +7,12 @@ class Image {
         //wyciągnij z oryginalnej nazwy pliku rozszerzenie
         $extension = pathinfo($name, PATHINFO_EXTENSION);
         //wygeneruj unikatowy string na podstawie nazwy pliku i aktualnego czasu
-        $name = sha1($name.microtime().".".$extension);
+        $name = sha1($name.microtime()).".".$extension;
         //pobierz ścieżkę pliku tymczasowego
         $tempPath = $files['tmp_name'];
+        //wygeneruj nową ścieżkę pliku
         $path = "uploads/".$name; 
+        //przenieś plik z folderu tymczasowego do folderu z uploadami
         move_uploaded_file($tempPath, $path);
     }
 }
